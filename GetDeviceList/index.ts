e05d5d5325e934131652c087fd59be45d5c905d1
pronk02 <jmcdonald@hmsmarketingservices.com>
2022-07-28 05:48:09 -0400

    Test

--- GetDeviceList/index.ts
+++ GetDeviceList/index.ts
@@ -6,7 +6,7 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
     const responseMessage = name
         ? "Hello, " + name + ". This HTTP triggered function executed successfully."
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-    //Test
+    //Test Again
     context.res = {
         // status: 200, /* Defaults to 200 */
         body: responseMessage
