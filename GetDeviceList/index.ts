357b4c9a9c3e219923f25c4487ed52186828b62a
pronk02 <jmcdonald@hmsmarketingservices.com>
2022-07-28 06:17:22 -0400

    Add lightOn

--- GetDeviceList/index.ts
+++ GetDeviceList/index.ts
@@ -1,17 +1,33 @@
-import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import {AzureFunction, Context, HttpRequest} from "@azure/functions";
+import Govee from "node-govee-led"
 
-const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    context.log('HTTP trigger function processed a request.');
-    const name = (req.query.name || (req.body && req.body.name));
+function lightOn() {
+
+    const GoveeClient = new Govee({
+        apiKey: process.env.GOVEE_API_KEY,
+        mac: process.env.TABLE_LIGHT_MAC_ADDRESS,
+        model: process.env.TABLE_LIGHT_MODEL,
+    });
+    
+    GoveeClient.turnOn
+}
+
+export const httpTrigger: AzureFunction = async (
+    context: Context,
+    req: HttpRequest
+): Promise<void> => {
+    context.log("HTTP trigger: Turn On.");
+    const name = req.query.name || (req.body && req.body.name);
     const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
+        ? "Hello, " +
+          name +
+          ". This HTTP triggered function executed successfully."
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
     //Test Again
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: responseMessage
+        body: responseMessage,
     };
-
 };
 
 export default httpTrigger;
